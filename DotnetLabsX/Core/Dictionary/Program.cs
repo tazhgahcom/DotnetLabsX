861f6a74819e630f62f3da7fe66e09741b7c4dd3
Tazhgah <tazhgahcom@gmail.com>
2026-09-21 15:02:25 +0330

    feat: Dictionary

---
+++ DotnetLabsX/Core/Dictionary/Program.cs
@@ -0,0 +1,7 @@
+﻿Dictionary<string, List<string>> wishlist = new();
+
+wishlist.Add("siavash", new List<string>() { "C#", "WordPress", "Database" });
+wishlist.Add("soroush", new List<string>() { "3D Max", "3D Modeling" });
+wishlist.Add("younes", new List<string>() { "C#", "PHP", "Golang" });
+
+Console.WriteLine(wishlist.Count);
---
+++ DotnetLabsX/Core/ListPatterns/Program.cs
@@ -0,0 +1,8 @@
+﻿string csv = "Siavash, Ebrahimi, 33, Yadollah";
+string[] info = csv.Split(",");
+
+Console.WriteLine(csv);
+if (info is [var firstName, var lastName, ..])
+{
+    Console.WriteLine($"{firstName} {lastName}");
+}
